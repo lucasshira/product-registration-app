af055055238597aa7ea9a39ee555a67be8bc8b4d
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-02-13 22:53:50 -0300

    feat: add toast error

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,50 @@ import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } 
 import { DialogFooter, DialogHeader } from "./components/ui/dialog";
 import { Label } from "@radix-ui/react-label";
 import { DialogClose } from "@radix-ui/react-dialog";
+import { Toaster } from "@/components/ui/toaster"
+import { useToast } from "./components/ui/use-toast";
+
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+
+
+interface Product {
+  id: string;
+  name: string;
+  price: string;
+}
 
 export function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [productName, setProductName] = useState<string>('');
+  const [productPrice, setProductPrice] = useState<string>('');
+  const { toast } = useToast();
+
+  const formatID = (id: string) => {
+    return id.substr(0, 8);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!productName || !productPrice) {
+      toast({
+        description: "Preencha todos os campos para prosseguir",
+      })
+      return;
+    }
+
+    const newProduct: Product = {
+      id: uuidv4(),
+      name: productName,
+      price: productPrice
+    };
+
+    setProducts(prevProducts => [...prevProducts, newProduct]);
+    setProductName('');
+    setProductPrice('');
+  };
+
   return (
       <div className="p-6 max-w-4xl mx-auto space-y-4">
         <h1 className="text-3xl font-bold">Produtos</h1>
@@ -36,15 +78,15 @@ export function App() {
                   <DialogDescription>Criar um novo produto no sistema</DialogDescription>
                 </DialogHeader>
 
-                <form className="space-y-6">
+                <form onSubmit={handleSubmit} className="space-y-6">
                   <div className="grid grid-cols-4 items-center text-right gap-2">
                     <Label htmlFor="name">Produto</Label>
-                    <Input className="col-span-3" id="name" />
+                    <Input className="col-span-3" id="name" value={productName} onChange={(e) => setProductName(e.target.value)} />
                   </div>
 
                   <div className="grid grid-cols-4 items-center text-right gap-2">
                     <Label htmlFor="price">Preço</Label>
-                    <Input className="col-span-3" id="price" />
+                    <Input className="col-span-3" id="price" value={productPrice} onChange={(e) => setProductPrice(e.target.value)} />
                   </div>
 
                   <DialogFooter>
@@ -66,17 +108,16 @@ export function App() {
               <TableHead>Preço</TableHead>
             </TableHeader>
             <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <TableRow key={i}>
-                  <TableCell>1982731892</TableCell>
-                  <TableCell>Produto {i}</TableCell>
-                  <TableCell>R$ 192,00</TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
+                {products.map(product => (
+                  <TableRow key={product.id}>
+                    <TableCell>{formatID(product.id)}</TableCell>
+                    <TableCell>{product.name}</TableCell>
+                    <TableCell>R$: {product.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
         </Table>
+        <Toaster />
       </div>
     </div>
   )
