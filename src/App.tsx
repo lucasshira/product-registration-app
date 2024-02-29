eb7886840b777b0c7b3d5d6f3ef77cf8c5bb27bb
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-02-29 15:45:01 -0300

    feat: add conditions

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import { Toaster } from "@/components/ui/toaster"
 import { useToast } from "./components/ui/use-toast";
 
 import { v4 as uuidv4 } from 'uuid';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
+import NenhumProduto from "./components/NenhumProduto";
 
 interface Product {
   id: string;
@@ -23,6 +24,10 @@ export function App() {
   const [productName, setProductName] = useState<string>('');
   const [productPrice, setProductPrice] = useState<string>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [id, setId] = useState<string>('');
+  const [nome, setNome] = useState<string>('');
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
   const { toast } = useToast();
 
   const formatID = (id: string) => {
@@ -63,6 +68,38 @@ export function App() {
     localStorage.setItem('products', JSON.stringify(updatedProducts));
   };
 
+  const handleFilterProducts = () => {
+    if (!id && !nome) {
+      toast({
+        description: "ID e/ou nome de produto nao encontrado(s)",
+      })
+      return;
+    }
+
+    const filtered = products.filter(product => {
+      const idMatch = id ? product.id.includes(id) : true;
+      const nomeMatch = nome ? product.name.toLocaleLowerCase().includes(nome.toLocaleLowerCase()) : true;
+
+      return idMatch && nomeMatch;
+    });
+
+    if (filtered.length === 0) {
+      toast({
+        description: "Nenhum produto encontrado com os critérios de busca informados"
+      });
+    }
+
+    setFilteredProducts(filtered);
+  };
+
+  const handleChangeId = (e: ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const handleChangeNome = (e: ChangeEvent<HTMLInputElement>) => {
+    setNome(e.target.value);
+  };
+
   useEffect(() => {
     const savedProducts = localStorage.getItem('products');
     if (savedProducts) {
@@ -76,9 +113,9 @@ export function App() {
 
           <div className="flex items-center justify-between">
             <form className="flex items-center gap-2">
-              <Input type="id" placeholder="ID do pedido" />
-              <Input type="name" placeholder="Nome do produto" />
-              <Button type="submit" variant={"link"}>
+              <Input type="id" placeholder="ID do pedido" onChange={handleChangeId} />
+              <Input type="name" placeholder="Nome do produto" onChange={handleChangeNome} />
+              <Button type="submit" variant={"link"} onClick={(e) => {e.preventDefault(); handleFilterProducts(); }}>
                 <Search className="w-4 h-4 mr-2" />
                 Filtar resultados
               </Button>
@@ -123,22 +160,41 @@ export function App() {
           <div className="border rounded-lg p-2">
           <Table>
             <TableHeader>
-              <TableHead>ID</TableHead>
-              <TableHead>Produto</TableHead>
-              <TableHead>Preço</TableHead>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>Produto</TableHead>
+                <TableHead>Preço</TableHead>
+              </TableRow>
             </TableHeader>
             <TableBody>
-                {products.map(product => (
-                  <TableRow key={product.id}>
-                    <TableCell>{formatID(product.id)}</TableCell>
-                    <TableCell>{product.name}</TableCell>
-                    <TableCell className="flex justify-between">
-                      R$: {product.price}
-                      <LucideClipboardX className="cursor-pointer" onClick={() => handleDeleteItem(product.id)} />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
+              {(!products.length && !filteredProducts.length) ? (
+                <NenhumProduto />
+              ) : (
+                filteredProducts.length > 0 ? (
+                  filteredProducts.map(product => (
+                    <TableRow key={product.id}>
+                      <TableCell>{formatID(product.id)}</TableCell>
+                      <TableCell>{product.name}</TableCell>
+                      <TableCell className="flex justify-between">
+                        R$: {product.price}
+                        <LucideClipboardX className="cursor-pointer" onClick={() => handleDeleteItem(product.id)} />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  products.map(product => (
+                    <TableRow key={product.id}>
+                      <TableCell>{formatID(product.id)}</TableCell>
+                      <TableCell>{product.name}</TableCell>
+                      <TableCell className="flex justify-between">
+                        R$: {product.price}
+                        <LucideClipboardX className="cursor-pointer" onClick={() => handleDeleteItem(product.id)} />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                )
+              )}
+            </TableBody>  
           </Table>
         <Toaster />
       </div>
---
+++ src/components/NenhumProduto.tsx
@@ -0,0 +1,9 @@
+const NenhumProduto = () => {
+  return (
+  <div className="font-medium mx-6 my-6">
+    <h2>Nenhum produto adicionado na base ainda.</h2>
+  </div>
+  );
+}
+
+export default NenhumProduto;
