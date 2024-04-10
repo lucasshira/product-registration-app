9e7bf5fcb64b00a186e6b4378ecf552c2aead968
Lucas Shirabayashi Azevedo <144144880+lucasshira@users.noreply.github.com>
2024-04-10 20:51:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Separator } from "@/components/ui/separator"
 import { Progress } from "@/components/ui/progress"
 import GoogleLoginAuth from "./components/GoogleLoginAuth";
@@ -7,20 +7,11 @@ import NotLogged from './components/NotLogged';
 
 export function App() {
   const [userSub, setUserSub] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleLoginSuccess = (sub: string) => {
     setUserSub(sub);
-    setIsLoading(true);
   }
 
-  useEffect(() => {
-    if (isLoading) {
-      const timer = setTimeout(() => setIsLoading(false), 1500);
-      return () => clearTimeout(timer);
-    }
-  }, [isLoading])
-  
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
       <div className="flex flex-row-reverse justify-between">
@@ -28,16 +19,10 @@ export function App() {
         <a href={'#'} className="text-4xl font-bold">Produtos</a>
       </div>
       <Separator />
-      {isLoading ? (
-        <Progress value={100} className="w-[50%] mx-auto block" style={{ marginTop: '60px' }} />
+      {!userSub ? (
+        <NotLogged />
       ) : (
-        <>
-          {!userSub ? (
-            <NotLogged />
-          ) : (
-            <Products userSub={userSub} />
-          )}
-        </>
+        <Products userSub={userSub} />
       )}
     </div>
   )
