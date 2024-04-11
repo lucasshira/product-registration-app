965c45b8af1203ef6e4a0fadb5a657421ee6af4c
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-04-11 00:50:03 -0300

    fix loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Separator } from "@/components/ui/separator"
 import GoogleLoginAuth from "./components/GoogleLoginAuth";
 import Products from "./components/Products";
@@ -7,7 +7,6 @@ import Loading from './components/Loading';
 
 export function App() {
   const [userSub, setUserSub] = useState<string | null>(null);
-<<<<<<< HEAD
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleLoginSuccess = (sub: string) => {
@@ -21,13 +20,6 @@ export function App() {
     }
   }, [loading, userSub])
 
-=======
-
-  const handleLoginSuccess = (sub: string) => {
-    setUserSub(sub);
-  }
-
->>>>>>> 1bfe0f81304f85733ffb2125b41c7a91907f4455
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
       <div className="flex flex-row-reverse justify-between">
@@ -35,17 +27,10 @@ export function App() {
         <a href={'#'} className="text-4xl font-bold">Produtos</a>
       </div>
       <Separator />
-<<<<<<< HEAD
       {loading ? (
         <Loading size={2} />
       ) : (
         userSub ? <Products userSub={userSub} /> : <NotLogged />
-=======
-      {!userSub ? (
-        <NotLogged />
-      ) : (
-        <Products userSub={userSub} />
->>>>>>> 1bfe0f81304f85733ffb2125b41c7a91907f4455
       )}
     </div>
   )
