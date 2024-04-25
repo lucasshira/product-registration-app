b7150974027581f82ec5d43b3eb151b3bdf28308
Lucas Shirabayashi Azevedo <144144880+lucasshira@users.noreply.github.com>
2024-04-25 10:41:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,13 @@ import { Separator } from "@/components/ui/separator"
 import GoogleLoginAuth from "./components/GoogleLoginAuth";
 import Products from "./components/Products";
 import NotLogged from './components/NotLogged';
-import Loading from './components/Loading';
 
 export function App() {
   const [userSub, setUserSub] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   const handleLoginSuccess = (sub: string) => {
     setUserSub(sub);
-    setLoggedIn(true);
   }
-
-  useEffect(() => {
-    if (userSub === null && loggedIn) {
-      setLoading(true);
-    } else {
-      setLoading(false);
-    }
-  }, [userSub, loggedIn])
 
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
@@ -30,11 +18,7 @@ export function App() {
         <a href={'#'} className="text-4xl font-bold">Produtos</a>
       </div>
       <Separator />
-      {loading ? (
-        <Loading size={2} />
-      ) : (
-        userSub ? <Products userSub={userSub} /> : <NotLogged />
-      )}
+      {userSub ? <Products userSub={userSub} /> : <NotLogged />}
     </div>
   )
 }
