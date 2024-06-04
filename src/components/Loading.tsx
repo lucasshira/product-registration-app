827c85abca6e4a141ac553c6a65256ab9e03017c
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-06-04 19:48:49 -0300

    fix: loading animation when adding new item

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,7 +7,7 @@ const Loading = (props: LoadingProps) => {
   return (
     <div className="flex justify-center items-center w-full h-full">
       <div className={props.size === 2 ? `animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 ${props.darkMode ? 'border-gray-100' : 'border-gray-800'} mt-5` : 
-        `animate-spin rounded-full h-5 w-5 border-t-4 border-b-4 ${props.darkMode ? 'border-gray-800' : 'border-gray-100'}`}>
+        `animate-spin rounded-full h-5 w-5 border-t-4 border-b-4`}>
         </div>
     </div>
   );
