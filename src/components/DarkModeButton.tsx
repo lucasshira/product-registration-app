c358976ba152c5ff71627bf06d211701e0977e28
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-07-18 23:15:36 -0300

    fix: toggle dark mode button style

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -6,23 +6,19 @@ interface DarkModeButton {
 }
 
 export default function DarkModeButton(props: DarkModeButton) {
-  return props.theme === 'dark' ? (
-    <div onClick={props.changeTheme} className="hidden sm:flex items-center cursor-pointer
-    bg-gradient-to-r from-yellow-300 to-yellow-600 w-14 h-8 p-1 rounded-full">
-      <div className="flex items-center justify-center bg-white text-yellow-600 size-6 rounded-full transition-all duration-300 transform translate-x-0">
-        {SunIcon}
-      </div>
-      <div className="hidden lg:flex items-center ml-2 text-white">
-      </div>
-    </div>
-  ) : (
-    <div onClick={props.changeTheme} className="hidden sm:flex justify-end items-center cursor-pointer
-    bg-gradient-to-r from-gray-500 to-gray-900 w-14 h-8 p-1 rounded-full">
-      <div className="hidden lg:flex items-center mr-2 text-white">
-      </div>
-      <div className="flex items-center justify-center bg-black text-gray-300 size-6 rounded-full transition-all duration-300 transform translate-x-6">
-        {MoonIcon}
+  return (
+    <div
+      onClick={props.changeTheme}
+      className="hidden sm:flex items-center cursor-pointer
+      bg-gradient-to-r from-yellow-300 to-yellow-600 dark:from-gray-500 dark:to-gray-900 w-14 h-8 p-1 rounded-full relative transition-all duration-300"
+    >
+      <div
+        className={`flex items-center justify-center w-6 h-6 bg-white dark:bg-black text-yellow-600 dark:text-gray-300 rounded-full transition-all duration-300 transform ${
+          props.theme === 'dark' ? 'translate-x-0' : 'translate-x-6'
+        }`}
+      >
+        {props.theme === 'dark' ? SunIcon : MoonIcon}
       </div>
     </div>
-  )
-};
+  );
+}
